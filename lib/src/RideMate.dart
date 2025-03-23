1e99df1392abe59afaed6880e151447f1596d7f0
fixedbyte <wilmerquispecode@gmail.com>
2025-03-23 12:53:23 -0500

    feat(): edit .gitignore

---
+++ lib/src/RideMate.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RideMate extends StatelessWidget {
+    const RideMate({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+        return WidgetsApp(title: "Ride Mate", color: const Color(255),);
+    }
+}
